e18bf86224872842558e0184f10259e2c82bb84f
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-25 12:42:58 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- os/linux/src/linux.cpp
+++ os/linux/src/linux.cpp
@@ -1,15 +1,1 @@
 #include "os.hpp"
-
-__attribute__((weak)) int main(int argc, char* argv[]) {
-    arg(0, argv[0]);
-    setup();
-    for (int i = 1; i < argc; i++) {  //
-        arg(i, argv[i]);
-    }
-    loop();
-    return 0;
-}
-
-__attribute__((weak)) void arg(int argc, char* argv) {  //
-    fprintf(stderr, "arg[%i] = <%s>\n", argc, argv);
-}
