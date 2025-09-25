9a883311293ba0ab40f41a4217096827bde507cc
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-25 17:53:22 +0400

    .

--- mk/doc.mk
+++ mk/doc.mk
@@ -4,3 +4,15 @@ doc:
 .PHONY: doxy
 doxy: .doxygen doc/DoxygenLayout.xml doc/logo.png
 	rm -rf doc/html ; doxygen $< 1>/dev/null
+
+REF += doc/compiler/Appel_compiling-with-continuations.pdf
+doc/compiler/Appel_compiling-with-continuations.pdf:
+	$(CURL) $@ https://scispace.com/pdf/compiling-with-continuations-16ejf5h952.pdf
+
+REF += doc/compiler/Nguyen_OCaml_LLVM.pdf
+doc/compiler/Nguyen_OCaml_LLVM.pdf:
+	$(CURL) $@ https://www.theseus.fi/bitstream/handle/10024/166119/Nguyen_Anh%20.pdf
+
+REF += doc/compiler/Sestoft_PLC.pdf
+doc/compiler/Sestoft_PLC.pdf:
+	$(CURL) $@ http://ijevanlib.ysu.am/wp-content/uploads/2017/12/plcsd-1-2.pdf
