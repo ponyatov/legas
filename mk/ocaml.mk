ec68a65d6b86a6f970a44631e2753d388a065a1b
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-25 17:26:35 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/ocaml.mk
+++ mk/ocaml.mk
@@ -1,4 +1,7 @@
 ocaml:
-	sudo apt install -yt opam ocaml dune utop
+	sudo apt install -yt opam ocaml dune
 	opam init
+	opam switch create 5.3.0
+# 	opam switch create $(OCAML_VER)
+	opam install utop
 # 	bash -c "sh <(curl -fsSL https://opam.ocaml.org/install.sh)"
