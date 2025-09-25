860458b18fd3f10c7770976eced7493753ae1389
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-25 16:27:35 +0400

    .

---
+++ mk/ref.mk
@@ -0,0 +1,3 @@
+REF += doc/compiler/Appel_compiling-with-continuations.pdf
+doc/compiler/Appel_compiling-with-continuations.pdf:
+	$(CURL) $@ https://scispace.com/pdf/compiling-with-continuations-16ejf5h952.pdf
